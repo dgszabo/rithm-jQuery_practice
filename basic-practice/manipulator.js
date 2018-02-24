d7e9fd4227dbdbe877fb51b75b6429d8c0be60bd
Daniel Szabo <dgszabo@gmail.com>
2018-02-23 19:34:10 -0800

    Exercises 4-5 completed

--- basic-practice/manipulator.js
+++ basic-practice/manipulator.js
@@ -5,4 +5,10 @@ $(document).ready(function() {
     $('article img').addClass('image-center');
     // 3
     $('p:last-of-type').remove()
+    // 4
+    var randomSize = Math.floor(Math.random() * 100);
+    $('h1[id="title"]').css('font-size', randomSize);
+    // 5
+    var newListElement = $('<li>').text('whatever you want');
+    $('ol').append(newListElement);
 });
