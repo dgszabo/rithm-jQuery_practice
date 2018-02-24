8416ad20ddfa203de6cfebb9612309f368cf9819
Daniel Szabo <dgszabo@gmail.com>
2018-02-23 19:21:40 -0800

    Exercises 1-3 completed

--- basic-practice/manipulator.js
+++ basic-practice/manipulator.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-    console.log('the JS file is online');
+    // 1
+    console.log('Let\'s get ready to party with jQuery!');
+    // 2
+    $('article img').addClass('image-center');
+    // 3
+    $('p:last-of-type').remove()
 });
